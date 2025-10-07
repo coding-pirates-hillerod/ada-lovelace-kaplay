5b398622ad7f6f0d99aa85f90b7188bcacffa726
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-10-07 13:51:18 +0200

    Added collision

---
+++ 05-collision/collision/src/main.js
@@ -0,0 +1,40 @@
+import kaplay from "kaplay";
+import "kaplay/global"; // uncomment if you want to use without the k. prefix
+
+kaplay();
+
+loadSprite("bean", "sprites/bean.png");
+
+scene("game", () => {
+  const bean = add([pos(120, 80), sprite("bean"), area(), body(), "bean"]);
+
+  const enemy = add([
+    pos(800, 400),
+    rect(100, 100),
+    area(),
+    color(1, 0, 0),
+    "enemy",
+  ]);
+
+  bean.onCollide("enemy", (enemy) => {
+    destroy(enemy);
+  });
+
+  bean.onKeyDown("left", () => {
+    bean.move(-150, 0);
+  });
+
+  bean.onKeyDown("right", () => {
+    bean.move(150, 0);
+  });
+
+  bean.onKeyDown("up", () => {
+    bean.move(0, -150);
+  });
+
+  bean.onKeyDown("down", () => {
+    bean.move(0, 150);
+  });
+});
+
+go("game");
